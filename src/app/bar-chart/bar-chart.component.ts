df2403b9caa4a61a1f1dfc61b9d9d1f046941f97
David Onwukwe <daveik55@gmail.com>
2022-07-20 14:46:35 +0100

    implement bar chart

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -6,10 +6,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bar-chart.component.scss']
 })
 export class BarChartComponent implements OnInit {
-
+  chartOptions: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.setChartOptions();
+  }
+
+  setChartOptions() {
+    this.chartOptions = {
+      responsive: true,
+      maintainAspectRatio: false,
+      xAxis: {
+        type: 'category',
+        data: ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May'],
+        boundaryGap: false,
+        axisLabel: {
+          textStyle: {
+            color: 'white'
+          }
+        },
+
+      },
+      yAxis: {
+        type: 'value',
+        splitNumber: 5,
+        axisLabel: {
+          textStyle: {
+            color: 'white'
+          },
+          padding: [0, -12, 0, -12]
+        },
+        splitLine: {
+          lineStyle: {
+            color: 'rgba(255, 255, 255, 0.2)',
+            width: 1,
+            type: 'dashed'
+          }
+        }
+      },
+      series: [
+        {
+          data: [0, 900, 700, 500, 1000, 70, 800],
+          type: 'bar',
+          barMaxWidth: 5,
+          barGap: '0%',
+          barCategoryGap: '0%',
+          itemStyle: {color: 'white'}
+        }
+      ]
+    };
   }
 
 }
